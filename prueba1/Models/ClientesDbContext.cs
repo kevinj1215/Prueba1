f22a52a27b03a4baebd561f146ebbfaf38621b4a
Kevin Mendoza <kevinmentobar05@hotmail.com>
2025-03-20 03:00:18 -0600

    Primera Actualizacion

---
+++ prueba1/Models/ClientesDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace prueba1.Models;
+
+public partial class ClientesDbContext : DbContext
+{
+    public ClientesDbContext()
+    {
+    }
+
+    public ClientesDbContext(DbContextOptions<ClientesDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Cliente> Clientes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-AKDEFF7\\SQLEXPRESS;Initial Catalog=ClientesDB;Integrated Security=True;Encrypt=False");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cliente>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Clientes__3214EC076AA7EFB3");
+
+            entity.Property(e => e.Direccion).HasMaxLength(255);
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+            entity.Property(e => e.Telefono).HasMaxLength(20);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ prueba1/Models/Script.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE ClientesDB;
+GO
+
+USE ClientesDB;
+GO
+
+CREATE TABLE Clientes (
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    Nombre NVARCHAR(100) NOT NULL,
+    Direccion NVARCHAR(255),
+    Telefono NVARCHAR(20),
+    Email NVARCHAR(100)
+);
